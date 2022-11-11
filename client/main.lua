85347e6b05fe81bc65fd1894f8bd980bae3419c3
GMW Scripts | Junior Developer <79488475+GermanWarthog@users.noreply.github.com>
2022-11-11 10:32:50 +0100

    v1.0.0 Release

---
+++ client/main.lua
@@ -0,0 +1,52 @@
+-- ════════════════════════════════════════════════════════════════════════════════════ --
+-- Debug Logs
+-- ════════════════════════════════════════════════════════════════════════════════════ --
+
+local filename = function()
+    local str = debug.getinfo(2, "S").source:sub(2)
+    return str:match("^.*/(.*).lua$") or str
+end
+print("^6[CLIENT - DEBUG] ^0: "..filename()..".lua gestartet");
+
+-- ════════════════════════════════════════════════════════════════════════════════════ --
+-- Code
+-- ════════════════════════════════════════════════════════════════════════════════════ --
+
+Recorder = {
+    IsRecording = false,
+}
+
+function Recorder:Main()
+    if Config.KeyBind.enable then 
+        if IsControlJustPressed(0, Config.KeyBind.key) and GetLastInputMethod(0) then 
+            if self.IsRecording then 
+                StopRecordingAndSaveClip()
+                self.IsRecording = false
+            else 
+                StartRecording(1)
+                self.IsRecording = true
+            end
+        end
+    end 
+end 
+
+RegisterCommand(Config.Command, function(source)
+    if Config.Command.enable then 
+        if self.IsRecording then 
+            StopRecordingAndSaveClip()
+            self.IsRecording = false
+        else 
+            StartRecording(1)
+            self.IsRecording = true
+        end
+    else 
+        print("Command is diesabled!")
+    end 
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        Recorder:Main()
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'German. Warthog'
+
+shared_scripts {'config.lua'}
+
+client_scripts {'client/main.lua'}
